476a93cced7532e2061eaedcde1a0a605eaf1895
AsthaMishra <mishraastha211@gmail.com>
2026-03-13 15:43:48 +0000

    tokio added

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,15 @@
 use paper_trading_backend::{AppState, config::DatabaseConfig};
-fn main() {
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>>{
     println!("Hello, world!");
 
     dotenvy::dotenv().ok();
-    let db = DatabaseConfig::from_env();
+
+    let db_confifg = DatabaseConfig::from_env();
+    let db = db_confifg.create_session().await?;
 
     let app_state = AppState::new(db);
+
+    Ok(())
 }
